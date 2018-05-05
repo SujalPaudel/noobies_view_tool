ad02c431a01e5509d9b82d7446a9164011dad47a
Velar <Sujal Paudel>
2018-05-05 19:42:20 +0545

    Added the feature of developer

--- lib/noobies_view_tool/renderer.rb
+++ lib/noobies_view_tool/renderer.rb
@@ -1,7 +1,10 @@
 module NoobiesViewTool
   class Renderer
     def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      "&copy; #{Time.now.year} | </b>#{name}</b> #{msg}".html_safe
+    end
+    def self.developer gitlink, name
+      '<a href = "gitlink">#{name}</a>'.html_safe
     end
   end
 end
