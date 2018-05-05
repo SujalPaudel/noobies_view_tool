314d04cf2a15254a2bf2a0a9c7909fb490688b9a
Velar <Sujal Paudel>
2018-05-05 21:54:23 +0545

    try1

--- lib/noobies_view_tool/renderer.rb
+++ lib/noobies_view_tool/renderer.rb
@@ -1,8 +1,11 @@
 module NoobiesViewTool
   class Renderer
-    def self.copyright name, msg, gitlink
+    def self.copyright name, msg
       "&copy; #{Time.now.year} | </b>#{name}</b> #{msg}".html_safe
+    end
+    def self.copyright gitlink
       "<a href = '#{gitlink}'>dsad</a>".html_safe
     end
+
   end
 end
